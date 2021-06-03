ccc55c4948d3d11d016a1edfa1514c3f416dff8b
Elsabeth Kidane <elsabethkidane@Elsabeths-MacBook-Pro.local>
2021-06-02 23:22:14 -0400

    new changes

--- js/index.js
+++ js/index.js
@@ -4,24 +4,16 @@ let thisYear=today.getFullYear()
 //var f = document.createElement("FOOTER");
 let footer = document.querySelector("footer");
 let copyright = document.createElement('p');
-copyright.innerHTML = `Elsabeth ${thisYear}`;
+copyright.innerHTML = `Elsabeth &copy${thisYear}`;
 footer.appendChild(copyright);
-
-
 let skills=['html','css','javscript','git','scrummaster','webdesign'];
-
 let skillsSection=document.getElementById("skills")
-//const skillsSection = document.getElementById('skills');
 const skillsList = skillsSection.getElementsByTagName('ul');
-//let skillsList=document.querySelector('ul')
-
 console.log(skills)
 for(let x=0 ;x <skills.length;x++){
     let skill = document.createElement('li');
-    
     skill.innerHTML=skills[x];
     //console.log(skill.innerHTML)
-    
 skillsSection.appendChild(skill)
     
 }
