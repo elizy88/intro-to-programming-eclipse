ccf12cc97699a974a0b98422401a130548d1af2e
Elsabeth Kidane <elsabethkidane@Elsabeths-MacBook-Pro.local>
2021-06-03 17:10:22 -0400

    my lesson

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 let today = new Date()
 console.log(today)
 let thisYear=today.getFullYear()
-//var f = document.createElement("FOOTER");
 let footer = document.querySelector("footer");
 let copyright = document.createElement('p');
 copyright.innerHTML = `Elsabeth ${thisYear}`;
@@ -13,10 +12,41 @@ let skills=['html','css','javscript','git','scrummaster','webdesign'];
 console.log(skills)
 for(let x=0 ;x <skills.length;x++){
     let skill = document.createElement('li');
-    
     skill.innerHTML=skills[x];
-    //console.log(skill.innerHTML)
-    
 skillsSection.appendChild(skill)
-    
 }
+let messageForm=document.getElementsByName('leave_message')
+leave_message.addEventListener('submit',function(e){
+    let Name=e.target.name.value
+    let Email=e.target.email.value
+    let Message=e.target.message.value
+    console.log(Name)
+    console.log(Email)
+    console.log(Message);
+    let messageSection = document.getElementById("messages");
+    let  messageList = document.querySelector('ul')
+    let newMessage = document.createElement('li');
+    e.preventDefault()
+newMessage.innerHTML=`<a href="mailto:elizit88@gmail.com">${Name}</a> wrote <span>${Message}</span>`
+
+console.log(newMessage)
+messages.appendChild(newMessage);
+let removeButton= document.createElement("button")
+removeButton.innerHTML = "remove";
+removeButton.type = "button";
+messages.appendChild(removeButton)
+removeButton.addEventListener("click",(e)=> {
+let entry = document.getElementById("messages").parentNode
+entry.remove();
+
+})
+})
+
+
+//    let entry = document.querySelector("button").parentNode.nodeName
+//    entry.remove();
+//   button.appendChild(newMessage)
+//   messages.appendChild(newMessage)
+
+// })
+// })
